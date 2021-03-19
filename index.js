e85f322b2974199d0a90aa3970542e231cfc673e
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-03-19 09:33:37 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function hitTheLights() {
         document.querySelector("H1").textContent = "Good Bye";
         document.querySelector("p").innerHTML = "<em>All done.</em>";
         document.querySelector("form label").textContent = "I've been Checked!";
-        document.querySelector("body").classList.add("opposite");
+        document.querySelector("html").classList.add("opposite");
         document.getElementsByTagName("div")[0].classList.add("opposite");
         document.querySelector("a").classList.remove("invisible");
         document.querySelector("i").classList.remove("fa-sun")
@@ -12,7 +12,7 @@ function hitTheLights() {
         document.querySelector("H1").textContent = "Hello";
         document.querySelector("p").innerHTML = "Here's a little something...";
         document.querySelector("form label").textContent = "Check the box!";
-        document.querySelector("body").classList.remove("opposite");
+        document.querySelector("html").classList.remove("opposite");
         document.getElementsByTagName("div")[0].classList.remove("opposite");
         document.querySelector("a").classList.add("invisible");
         document.querySelector("i").classList.add("fa-sun")
